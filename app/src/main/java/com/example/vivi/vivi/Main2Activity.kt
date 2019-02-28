39f48581c58438e335472461a87afd2b527d1e5c
vivianhds <vvnhenriquee@gmail.com>
2019-02-27 22:32:09 -0300

    Projeto Android

---
+++ app/src/main/java/com/example/vivi/vivi/Main2Activity.kt
@@ -0,0 +1,15 @@
+package com.example.vivi.vivi
+
+import android.os.Bundle
+import android.app.Activity
+
+import kotlinx.android.synthetic.main.activity_main2.*
+
+class Main2Activity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+
+}
---
+++ app/src/main/java/com/example/vivi/vivi/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.vivi.vivi
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+//import org.jetbrains.anko.toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+      //  val btLogin : Button = findViewById<Button>(R.id.btLogin)
+
+      //  btLogin.setOnClickListener(View.OnClickListener {
+       //     val textViewLogin: TextView = findViewById<TextView>(R.id.etUserName) as TextView
+       //     val textViewSenha: TextView = findViewById<TextView>(R.id.epSenha) as TextView
+
+         //   toast (textViewLogin.text)
+         //   )
+       // }
+    }
+
+}
